3a8cde3e8f0b92d4c0a4eb9440a5c7e7cb30f772
Chandra_Kurnia <candrakurniawan.dev@gmail.com>
2021-11-19 10:04:30 +0700

    setup scss, route

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="cover">
+    <div class="tittle">
+      <span class="name">CANDRA KURNIAWAN</span>
+      <span class="position">FULLSTACK WEB DEVELOPER</span>
+    </div>
+    <div class="imgProfile"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  mounted() {
+    document.title = 'Candra Kurniawan';
   },
 };
 </script>
+
+<style lang="scss">
+.cover {
+  width: 100vw;
+  height: 60vh;
+  background: url("../assets/images/cover.jpg");
+  background-position: left;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+
+  .tittle {
+    position: absolute;
+    top: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .name{
+      font-size: 30px;
+      color: white;
+    }
+
+    .position{
+      color: white;
+      font-size: 15px;
+    }
+  }
+
+  .imgProfile {
+    border-radius: 50%;
+    width: 230px;
+    height: 230px;
+    background-color: $primary;
+    margin-bottom: -115px;
+    border: 10px solid #ffff;
+  }
+}
+
+// X-Small devices (portrait phones, less than 576px)
+// No media query for `xs` since this is the default in Bootstrap
+
+// Small devices (landscape phones, 576px and up)
+@media (min-width: $sm) {
+  .cover{
+    .tittle{
+      .name{
+        color: red;
+      }
+    }
+  }
+ }
+
+// Medium devices (tablets, 768px and up)
+/* @media (min-width: 768px) {  } */
+
+// Large devices (desktops, 992px and up)
+/* @media (min-width: 992px) {  } */
+
+// X-Large devices (large desktops, 1200px and up)
+/* @media (min-width: 1200px) {  } */
+
+// XX-Large devices (larger desktops, 1400px and up)
+/* @media (min-width: 1400px) {  } */
+</style>
